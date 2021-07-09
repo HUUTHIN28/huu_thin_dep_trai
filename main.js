533ef1885077569495bb1e0b33e1c1bb5377c875
huu thin <huuthin30@gmail.com>
2021-07-09 06:32:22 -0700

    add main.js vao

---
+++ main.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app = express();
+const bodyParesr= require('body-parser');
+const port =process.env.PORT || 5000;
+const apiPath= '/api/';
+
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(express.static('cline'));
+//app.use(express.static('public'));
+//app.get('/',function(req,res){
+ // res.sendFile(__dirname + "/" + "cline/index.html");
+//});
+app.use(apiPath +'users',require('./routes/users.route'));
+//app.get('/thin',function(req,res){
+  //res.send('thin dep trai');
+//});
+app.listen(8080);
